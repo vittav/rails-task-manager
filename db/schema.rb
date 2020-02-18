51d62c032d183b9ca6698ec03916a8d3c9031511
Vitor Tavares <atavares.vitor@gmail.com>
2020-02-18 11:55:46 -0300

    Added column completed to Tasks with a default value false

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_145008) do
+ActiveRecord::Schema.define(version: 2020_02_18_145132) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
 end
